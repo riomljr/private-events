c82108a3671d0107273b007ab1de1c8e7efc8772
riomljr <22552727+riomljr@users.noreply.github.com>
2023-03-11 12:49:16 -0600

    add devise User model and new event form, styling

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,32 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
+  
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html {redirect_to root_path, notice: "Event was succesfully created."}
+      else
+        format.html { render :new}
+      end
+    end
+  end
+
+ 
+
+  private
+  
+  def event_params
+    params.require(:event).permit(:event_date, :name, :location)
+  end
+
+  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_230213) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_020510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_230213) do
     t.date "event_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "creator_id", null: false
+    t.string "location"
+    t.string "name"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_230213) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
